133bf9ce2aea74eb9cfc8b0d5c096edb61644408
Quinty <quintydewinter1@gmail.com>
2024-06-26 11:40:42 +0200

    alle migrations files runnen correct

--- database/migrations/2024_06_26_091109_create_q_contact_per_leverancier_table.php
+++ database/migrations/2024_06_26_091109_create_q_contact_per_leverancier_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
     {
         Schema::create('q_contact_per_leverancier', function (Blueprint $table) {
             $table->id();
-            $table->integer('leverancier_id');
-            $table->integer('contact_id');
+            $table->unsignedBigInteger ('leverancier_id');
+            $table->unsignedBigInteger ('contact_id');
             $table->timestamps();
 
-            $table->foreign('leverancier_id')->references('id')->on('leveranciers');
-            $table->foreign('contact_id')->references('id')->on('contacts');        });
+            $table->foreign('leverancier_id')->references('id')->on('q_leverancier');
+            $table->foreign('contact_id')->references('id')->on('q_contacts');        
+        });
     }
 
     /**
