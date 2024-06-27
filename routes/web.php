73132a0fd47192555d1b370654b382de7e840715
Shahed Amer <chanyeol6521354@gmail.com>
2024-06-27 09:07:37 +0200

    app/Http/Controllers/

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KlantenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+// route maken voor de homepagina
+Route::get('/klanten', [KlantenController::class, 'index'])->name('klanten.index');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
