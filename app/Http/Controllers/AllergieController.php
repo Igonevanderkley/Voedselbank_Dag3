ded0b63c6233013d33abb52953ee7b3abf433201
Igone van_der_Kley <igone@vdkley.nl>
2024-06-29 16:58:29 +0200

    Anyfylactic shock risk warning added to edit allergie functionality

--- app/Http/Controllers/AllergieController.php
+++ app/Http/Controllers/AllergieController.php
@@ -14,13 +14,11 @@ class AllergieController extends Controller
     public function read($gezinId)
     {
         try {
-            // Get the data for the specified gezin
             $gezinData = Gezin::where('id', $gezinId)->first();
             if (!$gezinData) {
                 throw new Exception("Gezin not found with id $gezinId");
             }
 
-            // Get all allergieen for the specified gezin
             $allergieenPerPersonen = Persoon::join('gezin', 'gezin_id', '=', 'gezin.id')
                 ->join('allergie_per_persoon', 'persoon_id', '=', 'persoon.id')
                 ->join('allergie', 'allergie_id', '=', 'allergie.id')
@@ -29,7 +27,6 @@ class AllergieController extends Controller
 
             return view('allergie_details', ['allergieenPerPersonen' => $allergieenPerPersonen, 'gezinData' => $gezinData]);
         } catch (\Exception $e) {
-            // Handle the exception (e.g., log the error, show an error message)
             return response()->view('errors.general', ['message' => $e->getMessage()], 500);
         }
     }
@@ -37,7 +34,6 @@ class AllergieController extends Controller
 
     public function update($allergieId, $persoonId)
     {
-        // Get all allergieen options
         $allergienOpties = Allergie::all();
         $gezinId = Persoon::select('gezin_id')->where('id', $persoonId)->first();
 
@@ -46,24 +42,27 @@ class AllergieController extends Controller
 
     public function edit(Request $request)
     {
-        // Get the allergieId and persoonId from the request
         $allergieId = $request->allergieId;
         $persoonId = $request->persoonId;
+        $huidigAllergieId = $request->huidigAllergieId;
 
-        // Get the gezinId from the persoonId
         $gezinId = Persoon::select('gezin_id')
             ->where('id', $persoonId)->first();
 
-        $allergiePerPersoon = AllergiePerPersoon::where('persoon_id', $persoonId)->first();
-        $allergiePerPersoon->allergie_id = $allergieId;
-        $allergiePerPersoon->save();
-
-
-        // Store the gezinId in the session
+        $risico = Allergie::select('anafylactisch_risico')->where('id', $huidigAllergieId)->value('anafylactisch_risico');
         session(['gezinId' => $gezinId->gezin_id]);
 
-        // Redirect to the allergie details page
-        return redirect()->route('wijzig_allergie', ['allergieId' => $allergieId, 'persoonId' => $persoonId])
-            ->with('status', 'De wijziging is doorgevoerd');
+
+        if ($risico == 'hoog') {
+            return redirect()->route('wijzig_allergie', ['allergieId' => $allergieId, 'persoonId' => $persoonId])
+                ->with('status', 'Voor het wijzigen van deze allergie wordt geadviseerd eerst een arts te raadplegen vanwege een hoog risico op een anafylactische shock.');
+        } else {
+            $allergiePerPersoon = AllergiePerPersoon::where('persoon_id', $persoonId)->first();
+            $allergiePerPersoon->allergie_id = $allergieId;
+            $allergiePerPersoon->save();
+
+            return redirect()->route('wijzig_allergie', ['allergieId' => $allergieId, 'persoonId' => $persoonId])
+                ->with('status', 'De wijziging is doorgevoerd');
+        }
     }
 }
